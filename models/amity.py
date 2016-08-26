0056204d91ff27196fa71cf24ee6cce15e0a74dd
andela-bmakinwa <makinwa37@gmail.com>
2016-08-26 16:45:44 +0100

    create rooms for amity

--- models/amity.py
+++ models/amity.py
@@ -1,6 +1,26 @@
+from docopt import docopt
+
 class Amity(object):
 
-    rooms = []
-
     def __init__(self):
-        print "Welcome to Amity"
+        print "Welcome to Amity\n"
+        self.rooms = []
+
+    def ready(self):
+        command = self.get_input()
+        method_params = map(str.lower, command.split())
+        getattr(self, method_params[0])(method_params[1::])
+        print self.rooms
+
+    def get_input(self):
+        return raw_input("What do you want to do?\n").strip()
+
+    """
+    Create rooms based on input
+    """
+    def create_room(self, rooms):
+        self.rooms = rooms
+
+if __name__ == '__main__':
+    amity = Amity()
+    amity.ready()
--- tests/models/test_amity.py
+++ tests/models/test_amity.py
@@ -4,13 +4,28 @@ from models.amity import Amity
 class TestAmityModel(unittest.TestCase):
 
     """
+    Test case for Amity model
+    """
+
+    """
         Set up test data
     """
     def setUp(self):
         self.hostel = Amity()
+
+    def get_input(self):
+        return "create_room ib lagos"
+
 
     """
         Verify is instance of Class is valid
     """
     def test_instance_is_valid(self):
         self.assertIsInstance(self.hostel, Amity, "Invalid instance")
+
+    """
+        Verify create_room creates a list of rooms
+    """
+    def test_create_method_populates_rooms(self):
+        self.hostel.create_room(["oyo", "ib", "lagos"])
+        self.assertEqual(["oyo", "ib", "lagos"], self.hostel.rooms)
