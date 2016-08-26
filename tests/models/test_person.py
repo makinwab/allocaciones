8509b76af13d9f536473ac14e3d6be2d1c42f1fd
andela-bmakinwa <makinwa37@gmail.com>
2016-08-26 13:21:20 +0100

    create person model test

---
+++ tests/models/test_person.py
@@ -0,0 +1,17 @@
+import unittest
+from models.person import Person
+
+class TestPersonModel(unittest.TestCase):
+    
+    #test set up
+    def setUp(self):
+        print ("Setting Up")
+        self.person_instance = Person()
+
+    # check that a true instance of Person was created
+    def test_instance_of_person(self):
+        self.assertIsInstance(self.person_instance, Person, "Is not an instance")
+
+
+if __name__ == '__main__':
+    unittest.main()
