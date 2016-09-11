07cfed7df59a8b6a5c7dbeacda9f0694d72b8696
andela-bmakinwa <makinwa37@gmail.com>
2016-09-11 19:41:43 +0100

    create rooms from base class

--- models/room.py
+++ models/room.py
@@ -1,2 +1,14 @@
+from random import randint
+
 class Room(object):
-    pass
+
+    def __init__(self):
+        self.rooms = None
+
+    def create(self, data):
+        self.rooms = data[1:]
+        #import pdb; pdb.set_trace()
+
+    def allocate(self, person):
+        number = randint(0,len(self.rooms))
+        print self.rooms[number]
