65ae3255a04084895e479cbcf53967d1effe50b7
andela-bmakinwa <makinwa37@gmail.com>
2016-09-15 21:00:18 +0100

    make prompt iterative

--- models/amity.py
+++ models/amity.py
@@ -1,26 +1,34 @@
 from docopt import docopt
+from room import Room
 from person import Person
 from office import Office
 from living_space import LivingSpace
+from staff import Staff
+from fellow import Fellow
 
 class Amity(object):
 
     def __init__(self):
         print "Welcome to Amity\n"
-        self.person = Person()
         self.office = Office()
         self.livingspace = LivingSpace()
+        self.staff = Staff()
+        self.fellow = Fellow()
 
     def ready(self):
-        # command = None
-        # while command != "exit":
-        command = self.get_input()
-        method_params = map(str.lower, command.split())
-        getattr(self, method_params[0])(method_params[1::])
+        command = None
+        while command != "exit":
+            command = self.get_input()
+            if command.lower() == "exit":
+                command = "exit"
+            else:
+                method_params = map(str.lower, command.split())
+                getattr(self, method_params[0])(method_params[1::])
+        return
 
 
     def get_input(self):
-        return raw_input("What do you want to do?\n").strip()
+        return raw_input(">> ").strip()
 
     """
      create_room OFFICE carat vvida minion
@@ -32,12 +40,19 @@ class Amity(object):
         else:
             self.livingspace.create(rooms)
 
+
     """
     add_person bukky FELLOW Y
     add_person jay STAFF
     """
-    def add_person(self, details):
-        self.person.add(details)
+    def add_person(self, person_details):
+        if len(person_details) == 2:
+            person_details.append("N")
+
+        if person_details[1] == "fellow":
+            self.fellow.add(person_details)
+        else:
+            self.staff.add(person_details)
 
 if __name__ == '__main__':
     amity = Amity()
