432174ed381b52b4730fdd1c6b582d6ab0ada0e1
andela-bmakinwa <makinwa37@gmail.com>
2016-09-15 21:19:46 +0100

    refactor test suite to cater for upgrades

--- tests/models/test_amity.py
+++ tests/models/test_amity.py
@@ -16,12 +16,12 @@ class TestAmityModel(unittest.TestCase):
     def test_instance_is_valid(self):
         self.assertIsInstance(self.hostel, Amity, "Invalid instance")
 
-    def test_create_method_populates_rooms(self):
+    def test_create_room_populates_rooms(self):
         self.hostel.create_room(["office", "ib", "lagos"])
-        self.assertEqual(["ib", "lagos"], self.hostel.office.rooms)
+        self.assertEqual({"ib": 6, "lagos": 6}, self.hostel.office.spaces)
 
     def test_add_person_creates_person_and_allocates_room(self):
         self.hostel.add_person(["bukky", "fellow", "Y"])
-        self.assertEqual("bukky", self.hostel.person.name)
-        self.assertEqual("FELLOW", self.hostel.person.role)
-        self.assertEqual("Y", self.hostel.person.accommodation)
+        self.assertEqual("bukky", self.hostel.fellow.name)
+        self.assertEqual("FELLOW", self.hostel.fellow.role)
+        self.assertEqual("Y", self.hostel.fellow.wants_accommodation)
