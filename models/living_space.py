472b7631229d9bb6e529c12a7ed0bd5f9c1c0954
andela-bmakinwa <makinwa37@gmail.com>
2016-09-15 21:00:37 +0100

    create living spaces

--- models/living_space.py
+++ models/living_space.py
@@ -4,3 +4,16 @@ class LivingSpace(Room):
 
     #can accomodate
     max_spaces = 4
+    spaces = {}
+
+    def create(self, data):
+        rooms = super(LivingSpace, self).create(data)["livingspace"]
+        for room in rooms:
+            self.spaces[room] = self.max_spaces
+
+        print self.spaces
+        print "Living spaces created"
+
+    @classmethod
+    def get_accomodation(cls, person):
+        self.allocations(person);
