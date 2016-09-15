fdb8bd558d9863bdb3e889e25c20abd0b90c086e
andela-bmakinwa <makinwa37@gmail.com>
2016-09-15 21:00:54 +0100

    create office spaces

--- models/office.py
+++ models/office.py
@@ -4,3 +4,12 @@ class Office(Room):
 
     # can accomodate
     max_spaces = 6
+    spaces = {}
+
+    def create(self, data):
+        rooms = super(Office, self).create(data)["office"]
+        for room in rooms:
+            self.spaces[room] = self.max_spaces
+
+        print self.spaces
+        print "Rooms created"
